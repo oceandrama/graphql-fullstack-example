b89ecf91b23df53ddda68ec6599cbc77a4e47613
Ruslan Baigunussov <oceandrama@gmail.com>
2020-03-27 22:50:07 +0300

    feat(server): Добавлена аутентификация

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,11 +1,34 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
+import cookie from "cookie";
+import { ContextParameters } from "graphql-yoga/dist/types";
+import { verify } from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-export interface Context {
+export interface Context extends ContextParameters {
   prisma: PrismaClient;
+  user: User | null;
 }
 
-export const createContext = {
-  prisma
-};
+interface Token {
+  userId: number;
+}
+
+export function getUser(request: Context["request"]) {
+  const token = cookie.parse(request.headers.cookie || "")["auth.token"];
+
+  if (token) {
+    const verifiedToken = verify(token, "supersecretkey") as Token;
+    return prisma.user.findOne({ where: { id: verifiedToken.userId } });
+  }
+
+  return null;
+}
+
+export const createContext = async (
+  params: ContextParameters
+): Promise<Context> => ({
+  ...params,
+  prisma,
+  user: await getUser(params.request)
+});
